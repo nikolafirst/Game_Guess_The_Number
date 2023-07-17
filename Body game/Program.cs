bc20e2577973c62d83962d4f2761b918a80cca0d
Nikolay Karasev <nomotorolla@gmail.com>
2023-07-17 17:33:42 +0300

    правки

--- Body game/Program.cs
+++ Body game/Program.cs
@@ -2,32 +2,37 @@
 
 
 
-Console.WriteLine("Меня зовут Алешка, я являюсь ИИ. Тебя как зовут?: ");
-string? Name = Console.ReadLine();
-Console.WriteLine("");
-Console.WriteLine($"Привет {Name}");
+using System.Runtime.CompilerServices;
 
-Console.WriteLine("Я хочу поиграть с тобой в занимательную игру, ты согласен? ДА/НЕТ");
+Console.Clear();
+
+Console.WriteLine("Меня зовут Антошка, я являюсь ИИ.");
+Console.WriteLine("Тебя как зовут?: ");
+string Name = Console.ReadLine();
+Console.WriteLine($"Привет {Name}");
+Console.WriteLine();
+
+Console.WriteLine($"{Name} , я хотел бы поиграть с тобой  в занимательную игру, ты согласен? Для продолжения нажми Enter, для выхода нажми 'q и Enter'");
 
 // Здесь должен обрабатываться запрос на ввод Да/Нет, но этого мы делать не будем. При нажатии "ENTER" игра продолжается
 
-Console.ReadLine();
+string positiveAnsver = Console.ReadLine();
 
 
 Console.WriteLine("Отлично, давай начнем!!! Я буду загадывать число от 1 до 5, а ты отгадывай. Есть одно НО, у тебя три попытки. Поехали :)");
 
-int user_Point = 3;
-int game_Point = 0;
-int Count = 0;
 
-while (user_Point > 0)
+
+int user_Step = 3;
+
+while (user_Step > 0)
 {
     Console.WriteLine("");  // Число игрока
-    int numA = int.Parse(Console.ReadLine()); 
+    int numA = int.Parse(Console.ReadLine());
     Random rnd = new Random();   // Загаданное число Алешкой
-    int numB = rnd.Next(1 , 6);
+    int numB = rnd.Next(1, 10);
     Console.WriteLine("Твое число: " + numA);
-    
+
     if (numA == numB)
     {
         Console.WriteLine("Поздравляю, я загадал число: " + numB);
@@ -36,10 +41,19 @@ while (user_Point > 0)
     else
     {
         Console.WriteLine("Не угадал, попробуй еще раз");
-        user_Point --;  // После каждой неудачной покупки минус одна попытка
-        continue;
+        user_Step--;  // После каждой неудачной  попытки минус одна попытка
     }
+
 
 }
 
-Console.WriteLine("Очень жаль, твои попытки закончились :((");
+if (user_Step == 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Очень жаль, твои попытки закончились :((");
+}
+
+Console.WriteLine();
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine("-------------- GAME OVER --------------------");
+Console.WriteLine("---------------------------------------------");
